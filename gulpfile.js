cbffb9ebccc667f6385129881b18698eb915701a
Augusto dos Anjos Almeida <augusto.almeida@serpro.gov.br>
2019-09-10 16:31:21 -0300

    tarefa para baixar logos dos tokens

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 const gulp = require('gulp');
-const download = require('gulp-download-stream');
+const download = require('gulp-download-files');
 const stripCssComments = require('gulp-strip-css-comments');
 const header = require('gulp-header');
+const decompress = require('gulp-decompress');
+const rename = require("gulp-rename");
 
 // Imported from Brand.ai
 const scssFile = '_tokens.scss';
 const scssFolder = 'base/';
 const scssURL = 'https://assets.brand.ai/dsgov/tema-principal/_style-params.scss?key=7QApNQG6j';
 
+const logosFolder = 'assets/images/';
+const logosURL = 'https://assets.brand.ai/dsgov/tema-principal/logos.zip?key=7QApNQG6j';
+
+
+gulp.task('get-logos', () => {
+  return download(logosURL)
+  .pipe(decompress())
+  .pipe(gulp.dest(logosFolder));
+});
+
 gulp.task('get-tokens', () => {
-  return download({
-    file: scssFile,
-    url: scssURL
-  })
+  return download(scssURL)
+  .pipe(rename(scssFile))
   .pipe(gulp.dest(scssFolder));
 });
 
@@ -26,4 +36,4 @@ gulp.task('clean-tokens', (done) => {
   done();
 });
 
-gulp.task('styles', gulp.series('get-tokens', 'clean-tokens'));
+gulp.task('styles', gulp.series('get-tokens', 'clean-tokens', 'get-logos'));
