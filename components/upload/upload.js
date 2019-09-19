dbae880f4eae4bdafbe844b4c01d5d59025668ec
70773807187 <samuel.brandao@serpro.gov.br>
2019-09-19 10:25:16 -0300

    Funcionalidade de adicionar e remover arquivos

--- components/upload/upload.js
+++ components/upload/upload.js
@@ -2,83 +2,100 @@ const inputElement = document.querySelector('.upload-input');
 const fileList = document.querySelector('.upload-file-list');
 const header = document.querySelector('.upload-header');
 const info = document.querySelector('.upload-info');
+const size = document.querySelector('.upload-size');
+const sizeNum = document.querySelector('.upload-size-num');
+const sizeBytes = document.querySelector('.upload-size-bytes');
+var fileArray = Array.from(inputElement.files);
 
 
 let dropbox;
 
 dropbox = document.querySelector('.br-upload');
-dropbox.addEventListener("dragenter", dragenter, false);
-dropbox.addEventListener("dragover", dragover, false);
+dropbox.addEventListener("dragenter", drag, false);
+dropbox.addEventListener("dragover", drag, false);
 dropbox.addEventListener("drop", drop, false);
 
 
 inputElement.addEventListener("change", handleFiles, false);
 
 function handleFiles(files) {
-  if (!files.length) {
-    files = this.files; 
-  } 
-  inputElement.files = files;
+
+   
+  newFiles = !files.length ? Array.from(inputElement.files) : Array.from(files);
+  fileArray = fileArray.concat(newFiles);
+
   info.style.display = 'none';
   header.innerHTML = 'Arquivos Selecionados';
-  updateSize();
-  
-  if (!files.length) {
-    fileList.innerHTML = "<p>Sem arquivos selecionados!</p>";
+  //console.log (fileArray);
+  updateFileList();
+}
+
+function updateFileList () {
+
+  if (!fileArray.length) {
+    fileList.innerHTML = "";
+    info.style.display = '';
+    header.innerHTML = 'Arraste e solte o(s) arquivo(s) do seu computador';
   } else {
     fileList.innerHTML = "";
     const list = document.createElement("ul");
     fileList.appendChild(list);
-    for (let i = 0; i < files.length; i++) {
+    for (let i = 0; i < fileArray.length; i++) {
       const li = document.createElement("li");
       list.appendChild(li);
       
       const info = document.createElement("span");
-      info.innerHTML = files[i].name ;
+      info.innerHTML = fileArray[i].name ;
       li.appendChild(info);
       
       const img = document.createElement("i");
       img.className = 'fa fa-times';
+      img.addEventListener("click", function(){removeFile(i, event)}, false);
       li.appendChild(img);
-      
     }
   }
+  updateSize();
+}
+
+function removeFile(index, e) {
+
+  e.stopPropagation();
+  e.preventDefault();
+  fileArray.splice(index,1);
+  //console.log (fileArray);
+  updateFileList();
 }
 
 function updateSize() {
-    let nBytes = 0,
-        oFiles = inputElement.files,
-        nFiles = oFiles.length;
-    for (let nFileId = 0; nFileId < nFiles; nFileId++) {
-      nBytes += oFiles[nFileId].size;
-    }
-    let sOutput = nBytes + " bytes";
-    // optional code for multiples approximation
-    for (let aMultiples = ["KB", "MB", "GB", "TB"], nMultiple = 0, nApprox = nBytes / 1024; nApprox > 1; nApprox /= 1024, nMultiple++) {
-      sOutput = nApprox.toFixed(3) + " " + aMultiples[nMultiple] ;
-    }
-    // end of optional code
-    document.getElementById("fileNum").innerHTML = nFiles;
-    document.getElementById("fileSize").innerHTML = sOutput;
+
+  let nBytes = 0,
+      oFiles = fileArray, 
+      nFiles = oFiles.length;
+  for (let nFileId = 0; nFileId < nFiles; nFileId++) {
+    nBytes += oFiles[nFileId].size;
+  }
+  
+  let sOutput = nBytes + " bytes";
+  for (let aMultiples = ["KB", "MB", "GB", "TB"], nMultiple = 0, nApprox = nBytes / 1024; nApprox > 1; nApprox /= 1024, nMultiple++) {
+    sOutput = nApprox.toFixed(3) + " " + aMultiples[nMultiple] ;
   }
 
-  function dragenter(e) {
-    e.stopPropagation();
-    e.preventDefault();
-  }
-  
-  function dragover(e) {
-    e.stopPropagation();
-    e.preventDefault();
-  }
+  size.style.visibility = nFiles>0 ? "visible" : "hidden";
+  sizeNum.innerHTML = nFiles;
+  sizeBytes.innerHTML = sOutput;
+}
 
-  function drop(e) {
-    e.stopPropagation();
-    e.preventDefault();
-  
-    const dt = e.dataTransfer;
-    const files = dt.files;
-  
-    handleFiles(files);
-  }
-  
+function drag(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function drop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  const dt = e.dataTransfer;
+  const files = dt.files;
+
+  handleFiles(files);
+}
