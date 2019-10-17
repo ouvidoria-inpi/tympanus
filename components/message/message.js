855911d280fd550149b3e097384b7e4ac98482f2
Augusto dos Anjos Almeida <augusto.almeida@serpro.gov.br>
2019-10-17 18:22:35 -0300

    inclusão do html e js de exemplo

---
+++ components/message/message.js
@@ -0,0 +1,28 @@
+class BRAlert {
+  
+  constructor(name, component) {
+    this.name = name;
+    this.component = component;
+    this._setBehavior();
+  }
+
+  _setBehavior() {
+    for (let button of this.component.querySelectorAll('div.close button')) {
+      button.addEventListener('click', () => {
+        this._dismiss(this.component);
+      });
+    }
+  }
+
+  _dismiss(component) {
+    component.parentNode.removeChild(component);
+  }
+}
+
+let alertList = [];
+
+window.onload = (function() {
+  for (let brAlert of window.document.querySelectorAll('.br-alert')) {
+    alertList.push(new BRAlert('br-alert', brAlert));
+  }
+})();
