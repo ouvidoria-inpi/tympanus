89b0d2f11002d9841c4ba6d3c7a184e94741967a
Tiago Alexandre Osorio da Silva - SUPSE/SESEP/SEDFE <tiago-alexandre.silva@serpro.gov.br>
2020-01-23 16:52:39 -0300

    corrigir menu lateral com clica na letra

--- dist/js/dsgov-components.js
+++ dist/js/dsgov-components.js
@@ -611,9 +611,10 @@ function toggleInputAction(element, className) {
 let collapseList = document.querySelectorAll('button[data-toggle="collapse"]');
 collapseList.forEach(function(collapse) {
   collapse.addEventListener("click", function(event) {
-    event.target.classList.toggle("is-open")
+		  this.classList.toggle("is-open")
   })
 })
+
 class BRAlert {
   constructor(name, component) {
     this.name = name;
@@ -1194,6 +1195,30 @@ window.onload = (function() {
 })()
 
 function documentReady(t){/in/.test(document.readyState)?setTimeout("documentReady("+t+")",9):t()}function findAncestor(t,e){for(;(t=t.parentElement)&&!t.classList.contains(e););return t}function unformatNumberString(t){return t=t.replace(/[^\d\.-]/g,""),Number(t)}function extractStringContent(t){var e=document.createElement("span");return e.innerHTML=t,e.textContent||e.innerText}function setColHeaderDirection(t,e,n){for(var r=0;r<n.length;r++)r==e?n[e].setAttribute("data-sort-direction",t):n[r].setAttribute("data-sort-direction",0)}function renderSortedTable(t,e){for(var n=t.getElementsByTagName("tbody")[0].getElementsByTagName("tr"),r=0;r<n.length;r++)for(var a=n[r].getElementsByTagName("td"),i=0;i<a.length;i++)a[i].innerHTML=e[r][i]}documentReady(function(){for(var t=document.getElementsByClassName("sortable-table"),e=[],n=0;n<t.length;n++)!function(){t[n].setAttribute("data-sort-index",n);for(var r=t[n].getElementsByTagName("tbody")[0].getElementsByTagName("tr"),a=0;a<r.length;a++)for(var i=r[a].getElementsByTagName("td"),o=0;o<i.length;o++)void 0===e[n]&&e.splice(n,0,[]),void 0===e[n][a]&&e[n].splice(a,0,[]),e[n][a].splice(o,0,i[o].innerHTML);for(var s=t[n].getElementsByTagName("thead")[0].getElementsByTagName("tr")[0].getElementsByTagName("th"),d=0;d<s.length;d++)!function(){var n=s[d].classList.contains("numeric-sort");s[d].setAttribute("data-sort-direction",0),s[d].setAttribute("data-sort-index",d),s[d].addEventListener("click",function(){var r=this.getAttribute("data-sort-direction"),a=this.getAttribute("data-sort-index"),i=findAncestor(this,"sortable-table").getAttribute("data-sort-index");setColHeaderDirection(1==r?-1:1,a,s),e[i]=e[i].sort(function(t,e){var i=extractStringContent(t[a]),o=extractStringContent(e[a]);return n&&(i=unformatNumberString(i),o=unformatNumberString(o)),i===o?0:1==r?i>o?-1:1:i<o?-1:1}),renderSortedTable(t[i],e[i])})}()}()});
+const tab = document.querySelectorAll('.br-tabs .item');
+if(tab){
+				for (let elem of tab) {
+								elem.addEventListener("click", function() { itemActiveSelected(elem) }, false);;
+				}
+}
+
+
+
+
+function itemActiveSelected(event) {
+    
+    const a = document.querySelectorAll('.upload-input');
+    const elements = event.parentElement.querySelectorAll('.br-tabs .item');
+				for (let elem of elements) {
+        elem.parentElement.querySelectorAll(".item")
+        elem.classList.remove("is-active");
+    }
+    event.classList.add("is-active");
+    
+
+
+}
+
 var parentEl
 var parentE2
 var nextEl
@@ -1244,30 +1269,6 @@ function setActive(el) {
 var target = document.getElementsByTagName('tr')
 for (i = 0; i < target.length; i++) {
   target[i].children[0].style.height = target[i].children[1].offsetHeight + 'px'
-}
-
-const tab = document.querySelectorAll('.br-tabs .item');
-if(tab){
-				for (let elem of tab) {
-								elem.addEventListener("click", function() { itemActiveSelected(elem) }, false);;
-				}
-}
-
-
-
-
-function itemActiveSelected(event) {
-    
-    const a = document.querySelectorAll('.upload-input');
-    const elements = event.parentElement.querySelectorAll('.br-tabs .item');
-				for (let elem of elements) {
-        elem.parentElement.querySelectorAll(".item")
-        elem.classList.remove("is-active");
-    }
-    event.classList.add("is-active");
-    
-
-
 }
 
 const inputElement = document.querySelector('.upload-input');
