f725b970ce0922b82f6edfc04d3f32ace9d2fec9
Augusto dos Anjos Almeida <augusto.almeida@serpro.gov.br>
2020-01-30 16:43:24 +0000

    inclusão dos tokens de icones

--- dist/js/dsgov-components.js
+++ dist/js/dsgov-components.js
@@ -3,13 +3,11 @@ class BRAccordeon {
     this.name = name;
     this.component = component;
     this._setBehavior();
-		}
-		
-		
+  }
 
   _setBehavior() {
-    for (let button of this.component.querySelectorAll("button.header")) {
-      button.addEventListener("click", event => {
+    for (let button of this.component.querySelectorAll('button.header')) {
+      button.addEventListener('click', event => {
         this._collapse(event);
         this._changeIcon(event);
       });
@@ -17,26 +15,26 @@ class BRAccordeon {
   }
 
   _collapse(event) {
-    for (let field of this.component.querySelectorAll(".item")) {
+    for (let field of this.component.querySelectorAll('.item')) {
       if (field === event.currentTarget.parentNode) {
-        field.classList.toggle("is-active");
-      } else if (field.classList.contains("is-active")) {
-        field.classList.toggle("is-active");
+        field.classList.toggle('is-active');
+      } else if (field.classList.contains('is-active')) {
+        field.classList.toggle('is-active');
       }
     }
   }
 
   _changeIcon(event) {
-    for (let field of this.component.querySelectorAll(".item")) {
-      if (field.classList.contains("is-active")) {
-        for (let icon of field.querySelectorAll(".icon")) {
-          icon.children[0].classList.remove("fa-plus");
-          icon.children[0].classList.add("fa-minus");
+    for (let field of this.component.querySelectorAll('.item')) {
+      if (field.classList.contains('is-active')) {
+        for (let icon of field.querySelectorAll('.icon')) {
+          icon.children[0].classList.remove('fa-plus');
+          icon.children[0].classList.add('fa-minus');
         }
       } else {
-        for (let icon of field.querySelectorAll(".icon")) {
-          icon.children[0].classList.remove("fa-minus");
-          icon.children[0].classList.add("fa-plus");
+        for (let icon of field.querySelectorAll('.icon')) {
+          icon.children[0].classList.remove('fa-minus');
+          icon.children[0].classList.add('fa-plus');
         }
       }
     }
@@ -46,8 +44,8 @@ class BRAccordeon {
 let accordeonList = [];
 
 window.onload = (function startBrAccordions() {
-  for (let brAccordeon of window.document.querySelectorAll(".br-accordeon")) {
-    accordeonList.push(new BRAccordeon("br-accordeon", brAccordeon));
+  for (let brAccordeon of window.document.querySelectorAll('.br-accordeon')) {
+    accordeonList.push(new BRAccordeon('br-accordeon', brAccordeon));
   }
 })();
 
