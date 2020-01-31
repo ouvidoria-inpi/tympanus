0bd3222ca38c733a845ab365256fdff12ba8c665
Tiago Alexandre Osorio da Silva - SUPSE/SESEP/SEDFE <tiago-alexandre.silva@serpro.gov.br>
2020-01-31 14:05:36 -0300

    mudança tab

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,22 +1,22 @@
-const tab = dropbox = document.querySelectorAll('.br-tabs .item');
+const tab = document.querySelectorAll( '.br-tabs .item' );
 
-for (let elem of tab) {
-    elem.addEventListener("click", function() { foi(elem) }, false);;
+for ( let elem of tab ) {
+	elem.addEventListener( "click", function () { changeElementTab( elem ) }, false );;
 
 }
 
 
-function foi(event) {
-    console.log(event);
-    const a = document.querySelectorAll('.upload-input');
-    const elements = event.parentElement.querySelectorAll('.br-tabs .item');
+function changeElementTab ( event ) {
+	console.log( event );
+	const a = document.querySelectorAll( '.upload-input' );
+	const elements = event.parentElement.querySelectorAll( '.br-tabs .item' );
 
-    for (let elem of elements) {
-        elem.parentElement.querySelectorAll(".item")
-        elem.classList.remove("is-active");
-    }
-    event.classList.add("is-active");
-    console.log(event.classList);
+	for ( let elem of elements ) {
+		elem.parentElement.querySelectorAll( ".item" )
+		elem.classList.remove( "is-active" );
+	}
+	event.classList.add( "is-active" );
+	console.log( event.classList );
 
 
 }
