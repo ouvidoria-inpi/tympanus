dec068953a110095979f8e9e1a2cef91d70e6ecf
Samuel <samuelbrandao@gmail.com>
2020-02-05 18:37:21 -0300

    Refactory da selecao, novo estado do checbox

--- src/js/components/table.js
+++ src/js/components/table.js
@@ -7,10 +7,36 @@
 // TODO: Barra superior - itens de ação e menu flutuante, tags de filtros, itens selecionados
 // TODO: Filtragem de cabeçalhos
 
-const brTables = document.querySelectorAll( ".br-table" );
 const brTablesHeadersClass = "headers";
-let active = "is-active";
-let brTableNumber = 0;
+const active = "is-active";
+var brTableNumber = 0;
+
+document.addEventListener("DOMContentLoaded", function(){
+
+	const brTables = document.querySelectorAll( ".br-table" );
+
+	for ( let brTable of brTables ) {
+		var searchBar = brTable.querySelector( ".search-bar" );
+		var searchTrigger = brTable.querySelector( ".search-trigger" );
+		var searchClose = brTable.querySelector( ".search-close" );
+		var responsive = brTable.querySelector( ".responsive" );
+		var headers = brTable.querySelector( "table thead tr" );
+		var rows = brTable.querySelectorAll( "table tbody tr" );
+
+		brTableNumber++;
+
+		setSyncScroll( responsive );
+		cloneHeader( brTable, headers );
+		setHeaderWidth( brTable, headers );
+		toogleSearch( searchBar, searchTrigger, searchClose );
+		setClickActions( brTable );
+		hoverRow( rows );
+
+		window.addEventListener( "resize", function () {
+			setHeaderWidth( brTable, headers );
+		} );
+	}
+});
 
 function hoverRow ( elements ) {
 	for ( let element of elements ) {
@@ -39,7 +65,7 @@ function setSyncScroll ( element ) {
 }
 
 function setHeaderWidth ( parent, element ) {
-	let cloneNode = parent.querySelector( `.${ brTablesHeadersClass }` );
+	var cloneNode = parent.querySelector( `.${ brTablesHeadersClass }` );
 	for ( let i = 0; i < element.children.length; i++ ) {
 		elementWidth = element.children[ i ].offsetWidth;
 		cloneElementWidth = cloneNode.children[ 0 ].children[ i ];
@@ -48,16 +74,16 @@ function setHeaderWidth ( parent, element ) {
 }
 
 function cloneHeader ( parent, element ) {
-	let clone = element.cloneNode( true );
-	let headersTag = document.createElement( "div" );
-	let scrollerTag = document.createElement( "div" );
+	var clone = element.cloneNode( true );
+	var headersTag = document.createElement( "div" );
+	var scrollerTag = document.createElement( "div" );
 
 	setSyncScroll( scrollerTag );
 	scrollerTag.classList.add( "scroller" );
 
 	for ( let i = 0; i < element.children.length; i++ ) {
-		let elementNode = clone.children[ i ].innerHTML;
-		let cloneElementNode = document.createElement( "div" );
+		var elementNode = clone.children[ i ].innerHTML;
+		var cloneElementNode = document.createElement( "div" );
 
 		cloneElementNode.classList.add( "item" );
 		cloneElementNode.innerHTML = elementNode;
@@ -66,8 +92,8 @@ function cloneHeader ( parent, element ) {
 
 		if ( cloneElementNode.children[ 0 ] ) {
 			if ( cloneElementNode.children[ 0 ].classList.contains( "br-checkbox" ) ) {
-				let cloneCheckbox = cloneElementNode.children[ 0 ];
-				let cloneCheckboxId = `${ brTablesHeadersClass }-${ parent.id }-check-all`;
+				var cloneCheckbox = cloneElementNode.children[ 0 ];
+				var cloneCheckboxId = `${ brTablesHeadersClass }-${ parent.id }-check-all`;
 				cloneCheckbox.querySelector( "input" ).id = cloneCheckboxId;
 				cloneCheckbox
 					.querySelector( "label" )
@@ -82,90 +108,126 @@ function cloneHeader ( parent, element ) {
 	parent.appendChild( headersTag );
 }
 
-function checkAll ( element, selectedBar ) {
-	let headerCheckbox = element.querySelector(
-		".headers [name='check'] [type='checkbox']"
-	);
-	let tableCheckboxes = element.querySelectorAll(
-		"tbody [name='check'] [type='checkbox']"
-	);
+function setClickActions ( brTable ) {
+	
+	var headerCheckbox = brTable.querySelector(".headers [name='check'] [type='checkbox']");
+	var tableCheckboxes = brTable.querySelectorAll("tbody [name='check'] [type='checkbox']");
+	var selectedBar = brTable.querySelector( ".selected-bar" );
+	var info_select_all = brTable.querySelector(".selected-bar .info .select-all");
 	
 	if ( tableCheckboxes ) {
 		for ( let checkbox of tableCheckboxes ) {
 			checkbox.addEventListener( "click", function () { 
-				countSelected( checkbox.checked ? 1 : -1, selectedBar, tableCheckboxes) 
-				if (!checkbox.checked) {
-					headerCheckbox.checked = false;
-				}
+				checkRow ( checkbox, selectedBar, tableCheckboxes, headerCheckbox );
 			} )
 		}
 	}
 
 	if ( headerCheckbox ) {
 		headerCheckbox.addEventListener( "click", function () {
-			count = tableCheckboxes.length;
-			if ( headerCheckbox.checked ) {
-				for ( let checkbox of tableCheckboxes ) {
-					checkbox.checked = true;
-				}
-			} else {
-				for ( let checkbox of tableCheckboxes ) {
-					checkbox.checked = false;
-				}
-				count = -1 * count;
-			}
-			countSelected(count, selectedBar, tableCheckboxes, headerCheckbox)
+			selectAllTable ( selectedBar, tableCheckboxes, headerCheckbox );
 		} );
+	}
+
+	if ( info_select_all ) {
+		info_select_all.addEventListener( "click", function () { 
+			selectAllTable ( selectedBar, tableCheckboxes );
+		});
 	}
 }
 
-function countSelected( count, selectedBar, tableCheckboxes, headerCheckbox ) {
-	let info_count = selectedBar.querySelector(".info .count");
-	let info_text = selectedBar.querySelector(".info .text");
-	let info_select_all = selectedBar.querySelector(".info .select-all");
-	let total = count < 2 ? parseInt(info_count.innerHTML, 10) + count : count;
-	
-	info_select_all.addEventListener( "click", function () { 
-		for ( let checkbox of tableCheckboxes ) {
-			checkbox.checked = false;
+function setRow ( checkbox, check ) {
+	tr = checkbox.parentNode.parentNode.parentNode;
+	if ( check ) {
+		tr.classList.add("is-selected");
+		checkbox.parentNode.classList.add("is-inverted");
+		checkbox.checked = true;
+	} else {
+		tr.classList.remove("is-selected");
+		checkbox.parentNode.classList.remove("is-inverted");
+		checkbox.checked = false;
+	}
+}
+
+function checkRow ( checkbox, selectedBar, tableCheckboxes, headerCheckbox){
+	check = checkbox.checked
+	setRow ( checkbox, check );
+	countSelected ( check ? 1 : -1, selectedBar, tableCheckboxes, headerCheckbox );
+}
+
+function checkAllRows ( tableCheckboxes ) {
+	for ( let checkbox of tableCheckboxes ) {
+		setRow(checkbox, true);
+	}
+}
+
+function uncheckAllRows ( tableCheckboxes ) {
+	for ( let checkbox of tableCheckboxes ) {
+		setRow(checkbox, false);
+	}
+}
+
+function selectAllTable ( selectedBar, tableCheckboxes, headerCheckbox ) {
+	count = tableCheckboxes.length;
+	var info_count = selectedBar.querySelector(".info .count");
+	var total = parseInt(info_count.innerHTML, 10);
+
+	if ( headerCheckbox ) { 
+		if ( headerCheckbox.checked ) {
+			checkAllRows ( tableCheckboxes )
+		} else {
+			uncheckAllRows ( tableCheckboxes )
+			count = -1 * count;
 		}
-		if ( headerCheckbox ) headerCheckbox.checked = false;
-		selectedBar.classList.remove("is-active");
-		total = 0;
-		info_count.innerHTML = total;
-		info_text.innerHTML = total > 1 ? "itens selecionados" : "item selecionado" 
-	});
+	} else {
+			if ( total == count ) {
+				uncheckAllRows ( tableCheckboxes )
+				count = -1 * count;
+			}
+			else {
+				checkAllRows ( tableCheckboxes )
+			}
+	}
+	countSelected(count, selectedBar, tableCheckboxes, headerCheckbox);
+}
+
+function countSelected( count, selectedBar, tableCheckboxes, headerCheckbox ) {
+	
+	var info_count = selectedBar.querySelector(".info .count");
+	var info_text = selectedBar.querySelector(".info .text");
+	var mobile_ico = selectedBar.querySelector(".info .select-all svg");
+	var total = count < 2 ? parseInt(info_count.innerHTML, 10) + count : count;
 
 	if ( total > 0 ) {
 		selectedBar.classList.add("is-active");
 		info_count.innerHTML = total;
-		info_text.innerHTML = total > 1 ? "itens selecionados" : "item selecionado" 
+		info_text.innerHTML = total > 1 ? "itens selecionados" : "item selecionado";
+		if ( headerCheckbox ) headerCheckbox.parentNode.classList.add("is-checking");
+		if (mobile_ico) {
+			mobile_ico.classList.add("fa-minus-square");
+			mobile_ico.classList.remove("fa-check-square");
+		}
+		if ( total == tableCheckboxes.length ) {
+			if ( headerCheckbox ) { 
+				headerCheckbox.checked = true;
+				headerCheckbox.parentNode.classList.remove("is-checking");
+			}
+			if (mobile_ico) {
+				mobile_ico.classList.remove("fa-minus-square");
+				mobile_ico.classList.add("fa-check-square");
+			}
+		}
 	}
 	else {
 		selectedBar.classList.remove("is-active");
 		info_count.innerHTML = 0;
+		if ( headerCheckbox ) { 
+			headerCheckbox.checked = false;
+			headerCheckbox.parentNode.classList.remove("is-checking");
+		}
+		if (mobile_ico) {
+			mobile_ico.classList.remove("fa-check-square");
+			mobile_ico.classList.add("fa-minus-square");
+		}
 	}
-}
-
-for ( let brTable of brTables ) {
-	let searchBar = brTable.querySelector( ".search-bar" );
-	let searchTrigger = brTable.querySelector( ".search-trigger" );
-	let searchClose = brTable.querySelector( ".search-close" );
-	let selectedBar = brTable.querySelector( ".selected-bar" );
-	let responsive = brTable.querySelector( ".responsive" );
-	let headers = brTable.querySelector( "table thead tr" );
-	let rows = brTable.querySelectorAll( "table tbody tr" );
-
-	brTableNumber++;
-
-	setSyncScroll( responsive );
-	cloneHeader( brTable, headers );
-	setHeaderWidth( brTable, headers );
-	toogleSearch( searchBar, searchTrigger, searchClose );
-	checkAll( brTable, selectedBar );
-	hoverRow( rows );
-
-	window.addEventListener( "resize", function () {
-		setHeaderWidth( brTable, headers );
-	} );
 }
