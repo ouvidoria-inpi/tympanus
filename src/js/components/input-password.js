43becda424202b6f89f7a8ad10dfcc50a2d5229a
Matheus <mth.monteiro@gmail.com>
2020-02-07 17:32:23 -0300

    Cria a classe BRInput com o comportamento de mostrar a senha

--- src/js/components/input-password.js
+++ src/js/components/input-password.js
@@ -1,23 +1,48 @@
-function toggleInputAction(element, className) {
-  for (
-    var inputAction = element;
-    !inputAction.classList.contains(className);
-    inputAction = inputAction.parentNode
-  );
+class BRInput {
 
-  var icon = element.getElementsByClassName("fas")[0];
-
-  if (icon.classList.contains("fa-eye")) {
-    inputAction
-      .querySelector("input[type='password'")
-      .setAttribute("type", "text");
-    icon.classList.remove("fa-eye");
-    icon.classList.add("fa-eye-slash");
-  } else if (icon.classList.contains("fa-eye-slash")) {
-    inputAction
-      .querySelector("input[type='text'")
-      .setAttribute("type", "password");
-    icon.classList.remove("fa-eye-slash");
-    icon.classList.add("fa-eye");
+  constructor(name, component) {
+    this.name = name;
+    this.component = component;
+    this._setBehavior();
+    console.log(this.component);
   }
+
+  _setBehavior() {
+    this._setPasswordViewBehavior();
+  }
+
+  _setPasswordViewBehavior() {
+    for (let inputPassword of this.component.querySelectorAll("input[type='password']")) {
+      if (inputPassword.parentNode.classList.contains("has-icon")) {
+        for (let buttonIcon of inputPassword.parentNode.querySelectorAll("button.icon")) {
+          buttonIcon.addEventListener("click", (event) => {
+            this._toggleShowPassword(event);
+          }, false)
+        }
+      }
+    }
+  }
+
+  _toggleShowPassword(event) {
+    for (let icon of event.currentTarget.querySelectorAll(".svg-inline--fa")) {
+      if (icon.classList.contains("fa-eye")) {
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+        for (let input of this.component.querySelectorAll("input[type='password']")) {
+          input.setAttribute("type", "text");
+        }
+      } else if (icon.classList.contains("fa-eye-slash")) {
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+        for (let input of this.component.querySelectorAll("input[type='text']")) {
+          input.setAttribute("type", "password");
+        }
+      }
+    }
+  }
+}
+
+let inputList = []
+for (let brInput of window.document.querySelectorAll(".br-input")) {
+  inputList.push(new BRInput("br-input", brInput));
 }
