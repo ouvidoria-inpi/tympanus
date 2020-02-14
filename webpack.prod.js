afa13881c80219fd88abb062e019898ec962a8c0
Natanael de Sousa Leite <natanael.leite@serpro.gov.br>
2020-02-14 15:07:57 -0300

    fix: ajusta scripts de execucao de compilaçao do dsgov usando webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,53 +1,53 @@
-const path = require('path');
+const path = require( 'path' );
 const isDEV = process.env.NODE_ENV === 'production'
-console.log('Estou no ***AMBIENTE de PRODUCTION***')
+console.log( 'Estou no ***AMBIENTE de PRODUCTION***' )
 
 // Webpack Stuff
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ConcatPlugin = require('webpack-concat-plugin');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+const ConcatPlugin = require( 'webpack-concat-plugin' );
+const HTMLWebpackPlugin = require( 'html-webpack-plugin' );
+const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
+const OptimizeCSSAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' )
 
 // We need Nodes fs module to read directory contents
-const fs = require('fs')
+const fs = require( 'fs' )
 
 const paths = {
-  src: path.resolve(__dirname, 'src'),
-  assets: path.resolve(__dirname, 'src', 'assets'),
-  fonts: path.resolve(__dirname, 'src', 'assets', 'fonts'),
-  images: path.resolve(__dirname, 'src', 'assets', 'images'),
-  dist: path.resolve(__dirname, 'dist'),
-  distjs: path.resolve(__dirname, 'dist', 'js'),
+  src: path.resolve( __dirname, 'src' ),
+  assets: path.resolve( __dirname, 'src', 'assets' ),
+  fonts: path.resolve( __dirname, 'src', 'assets', 'fonts' ),
+  images: path.resolve( __dirname, 'src', 'assets', 'images' ),
+  dist: path.resolve( __dirname, 'dist' ),
+  distjs: path.resolve( __dirname, 'dist', 'js' ),
 };
 
 // Our function that generates our html plugins
-function generateHtmlPlugins(templateDir, dirName) {
+function generateHtmlPlugins ( templateDir, dirName ) {
   // Read files in template directory
-  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir))
-  return templateFiles.map(item => {
+  const templateFiles = fs.readdirSync( path.resolve( __dirname, templateDir ) )
+  return templateFiles.map( item => {
     // Split names and extension
-    const parts = item.split('.')
-    const name = parts[0]
-    const extension = parts[1]
+    const parts = item.split( '.' )
+    const name = parts[ 0 ]
+    const extension = parts[ 1 ]
     // Create new HTMLWebpackPlugin with options
-    return new HTMLWebpackPlugin({
-      filename: `${dirName}/${name}.html`,
-      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+    return new HTMLWebpackPlugin( {
+      filename: `${ dirName }/${ name }.html`,
+      template: path.resolve( __dirname, `${ templateDir }/${ name }.${ extension }` ),
       templateParameters: {
-        version: JSON.stringify(require("./package.json").version).replace(/\"/gi, ""),
-        cdnUrl: require("./package.json").cdn
+        version: JSON.stringify( require( "./package.json" ).version ).replace( /\"/gi, "" ),
+        cdnUrl: require( "./package.json" ).cdn
       },
       inject: true,
-    })
-  })
+    } )
+  } )
 }
 
 // Call our function on our views directory.
-const htmlPluginsComponentes = generateHtmlPlugins('./src/pug/views/components', 'componentes')
-const htmlPluginsTemplates = generateHtmlPlugins('./src/pug/views/templates', 'templates')
+const htmlPluginsComponentes = generateHtmlPlugins( './src/pug/views/components', 'components' )
+const htmlPluginsTemplates = generateHtmlPlugins( './src/pug/views/templates', 'templates' )
 
 const fileLoader = {
   loader: 'file-loader',
@@ -55,15 +55,15 @@ const fileLoader = {
     name: '[name].[ext]',
 
     // Mantém a estrutura de diretórios (mas excluindo-se o 'src')
-    outputPath(filename, absoluteFilePath, absoluteRootPath) {
-      const relativePath = path.relative(absoluteRootPath, absoluteFilePath);
+    outputPath ( filename, absoluteFilePath, absoluteRootPath ) {
+      const relativePath = path.relative( absoluteRootPath, absoluteFilePath );
 
-      const outPath = relativePath.split('/');
+      const outPath = relativePath.split( '/' );
 
       // Remove a primeira parte do path, ou seja, 'src'
       outPath.shift();
 
-      return outPath.join('/');
+      return outPath.join( '/' );
     }
   }
 };
@@ -72,17 +72,17 @@ module.exports = {
   // mode: isDEV ? "development" : "production",
   mode: "production",
   entry: {
-    'dsgov': path.resolve(paths.src + "/scss", 'dsgov.scss'),
-    'dsgov-base': path.resolve(paths.src + "/scss", 'dsgov-base.scss'),
-    'dsgov-componentes': path.resolve(paths.src + "/scss", 'dsgov-components.scss'),
+    'dsgov': path.resolve( paths.src + "/scss", 'dsgov.scss' ),
+    'dsgov-base': path.resolve( paths.src + "/scss", 'dsgov-base.scss' ),
+    'dsgov-componentes': path.resolve( paths.src + "/scss", 'dsgov-components.scss' ),
   },
-  watch: true,
+  watch: false,
   output: {
     filename: './js/[name].js',
     path: paths.dist
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join( __dirname, 'dist' ),
     stats: 'errors-only',
     clientLogLevel: 'error',
     port: 9000,
@@ -116,12 +116,12 @@ module.exports = {
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)$/,
-        include: [paths.fonts],
+        include: [ paths.fonts ],
         loader: fileLoader,
       },
       {
         test: /\.(png|svg|jpg|jpg)$/,
-        include: [paths.images],
+        include: [ paths.images ],
         loader: fileLoader,
       },
       {
@@ -131,54 +131,54 @@ module.exports = {
           loader: 'pug-loader',
           query: {
             pretty: true,
-            root: path.resolve(__dirname, 'src/views')
+            root: path.resolve( __dirname, 'src/views' )
           }
         },
       },
     ]
   },
   resolve: {
-    extensions: [".js", ".scss"]
+    extensions: [ ".js", ".scss" ]
   },
   plugins: [
     // new CleanWebpackPlugin(),
-    new ConcatPlugin({
+    new ConcatPlugin( {
       uglify: true,
       sourceMap: true,
       name: 'dsgov-components',
       outputPath: 'js/',
       fileName: '[name].js',
-      filesToConcat: ['./src/js/components/**'],
+      filesToConcat: [ './src/js/components/**' ],
       attributes: {
         async: true
       }
-    }),
-    new ConcatPlugin({
+    } ),
+    new ConcatPlugin( {
       uglify: true,
       sourceMap: true,
       name: 'dsgov-templates',
       outputPath: 'js/',
       fileName: '[name].js',
-      filesToConcat: ['./src/js/templates/**'],
+      filesToConcat: [ './src/js/templates/**' ],
       attributes: {
         async: true
       }
-    }),
+    } ),
 
-    new CopyWebpackPlugin([
+    new CopyWebpackPlugin( [
       { context: 'src', from: '**/templates/*.js' },
       { context: 'src', from: '**/config/*.js' },
-    ]),
+    ] ),
     // Extract our css to a separate css file
-    new MiniCssExtractPlugin({
+    new MiniCssExtractPlugin( {
       filename: 'css/[name].min.css',
       chunkFilename: '[id].min.css',
       ignoreOrder: false, // Enable to remove warnings about conflicting order
-    }),
-  ].concat(htmlPluginsComponentes, htmlPluginsTemplates),
+    } ),
+  ].concat( htmlPluginsComponentes, htmlPluginsTemplates ),
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
+      new UglifyJsPlugin( {
         test: /\.js(\?.*)?$/i,
         cache: true,
         parallel: true,
@@ -188,8 +188,8 @@ module.exports = {
             comments: false,
           },
         },
-      }),
-      new OptimizeCSSAssetsPlugin({})
+      } ),
+      new OptimizeCSSAssetsPlugin( {} )
     ],
   },
   devtool: 'source-map'
