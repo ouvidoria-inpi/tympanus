eada83c9fea80cd27c60f3b17d35e1999cd751b9
Natanael de Sousa Leite <natanael.leite@serpro.gov.br>
2020-02-20 16:18:30 -0300

    fix: corrige a maneira de instanciar os componentes no projeto para evitar duplicaçao de declaraçao de classes

--- src/js/components/message.js
+++ src/js/components/message.js
@@ -1,25 +1,28 @@
 class BRAlert {
-  constructor(name, component) {
+  constructor ( name, component ) {
     this.name = name;
     this.component = component;
     this._setBehavior();
   }
 
-  _setBehavior() {
-    for (let button of this.component.querySelectorAll("div.close button")) {
-      button.addEventListener("click", () => {
-        this._dismiss(this.component);
-      });
+  _setBehavior () {
+    for ( let button of this.component.querySelectorAll( "div.close button" ) ) {
+      button.addEventListener( "click", () => {
+        this._dismiss( this.component );
+      } );
     }
   }
 
-  _dismiss(component) {
-    component.parentNode.removeChild(component);
+  _dismiss ( component ) {
+    component.parentNode.removeChild( component );
   }
 }
 
-let alertList = [];
+// let alertList = [];
 
-for (let brAlert of window.document.querySelectorAll(".br-message")) {
-  alertList.push(new BRAlert("br-message", brAlert));
-}
+// for ( let brAlert of window.document.querySelectorAll( ".br-message" ) ) {
+//   alertList.push( new BRAlert( "br-message", brAlert ) );
+// }
+
+
+export default BRAlert
