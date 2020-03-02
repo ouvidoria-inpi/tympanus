ce8bf1fb3dfc582abfb4b2ec0ee7207551f85eb3
Tiago Alexandre Osorio da Silva - SUPSE/SESEP/SEDFE <tiago-alexandre.silva@serpro.gov.br>
2020-03-02 09:52:59 -0300

    alteracao do cdn

--- cdn.sh
+++ cdn.sh
@@ -11,9 +11,8 @@ git clone git@git.serpro:ds-gov/cdn-dsgov-estaleiro.git
 mkdir $LATEST
 mkdir cdn-dsgov-estaleiro/htdocs/design-system/latest
 mkdir cdn-dsgov-estaleiro/htdocs/design-system/$LATEST
-echo "copiando++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo $LATEST > cdn-dsgov-estaleiro/version
 cp -r ../dist/** cdn-dsgov-estaleiro/htdocs/design-system/$LATEST
 cp -r ../dist/** cdn-dsgov-estaleiro/htdocs/design-system/latest
 cd ..
-
 git checkout cdn-publish
