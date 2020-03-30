ea8232c921c10e7cb42943bcd61aaf3e2485f129
Paula Coelho <paula.coelho@serpro.gov.br>
2020-03-30 15:06:03 -0300

    adicionando alteracoes na tag

---
+++ src/js/components/tag.js
@@ -0,0 +1,36 @@
+class BRTag {
+	constructor(name, component) {
+		this.name = name;
+        this.component = component;
+		this._setBehavior();
+	}
+
+    _setBehavior() {
+        for (let button of this.component.querySelectorAll("div.close button")) {
+            button.addEventListener("click", () => {
+                this.component.parentNode.removeChild(this.component);
+            });
+        }
+
+        for (let inputText of document.querySelectorAll('input[type="text"]')) {
+			inputText.addEventListener("keypress", event => {
+				var keycode = (event.keyCode ? event.keyCode : event.which);
+                if(keycode == '13'){
+                    alert(inputText.value);  
+                }
+                // impede o envio do campo
+                event.preventDefault();
+                event.stopPropagation();
+            });
+        }
+    }
+    
+}
+
+let tagList = []
+
+for (let brTag of window.document.querySelectorAll('.br-tag')) {
+	tagList.push(new BRTag('br-tag', brTag))
+}
+
+export default BRTag
