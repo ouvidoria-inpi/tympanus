abc124994b472aecb9fdf09ba608976aa26df64b
rafael.gomes@serpro.gov.br <Rafael Diego Gomes de Oliveira>
2020-04-06 17:36:47 -0300

    Removidos os console.log

--- src/js/components/breadcrumb.js
+++ src/js/components/breadcrumb.js
@@ -1,54 +1,53 @@
 class BRBreadcrumb {
 
-  constructor(name, component) {
+  constructor ( name, component ) {
     this.name = name;
     this.component = component;
-    console.log(this.component);
     this._setBehavior();
   }
 
-  _setBehavior() {
-    window.addEventListener("load", () => {
-      if(window.innerWidth < this.component.scrollWidth) {
+  _setBehavior () {
+    window.addEventListener( "load", () => {
+      if ( window.innerWidth < this.component.scrollWidth ) {
         this.hideCrumbs();
       }
-    });
-    this.component.addEventListener("mousewheel", (event) => {
-      let delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
-      event.currentTarget.scrollLeft -= (delta * 40);
+    } );
+    this.component.addEventListener( "mousewheel", ( event ) => {
+      let delta = Math.max( -1, Math.min( 1, ( event.wheelDelta || -event.detail ) ) );
+      event.currentTarget.scrollLeft -= ( delta * 40 );
       event.preventDefault();
-    });
+    } );
   }
 
-  hideCrumbs() {
-    for (let crumb of this.component.querySelectorAll("li:not(:nth-child(1)):not(:last-child)")) {
-      if (crumb.classList.contains("hidden")) {
-        crumb.classList.remove("hidden");
-        crumb.classList.add("more");
-        crumb.addEventListener("click", () => {
+  hideCrumbs () {
+    for ( let crumb of this.component.querySelectorAll( "li:not(:nth-child(1)):not(:last-child)" ) ) {
+      if ( crumb.classList.contains( "hidden" ) ) {
+        crumb.classList.remove( "hidden" );
+        crumb.classList.add( "more" );
+        crumb.addEventListener( "click", () => {
           this._showCrumbs();
-        });
+        } );
       } else {
-        crumb.classList.add("hidden");
+        crumb.classList.add( "hidden" );
       }
     }
   }
 
-  _showCrumbs() {
-    for (let crumb of this.component.querySelectorAll("li:not(:nth-child(1)):not(:last-child)")) {
-      if (crumb.classList.contains("more")) {
-        crumb.classList.remove("more");
-        crumb.classList.add("hidden");
+  _showCrumbs () {
+    for ( let crumb of this.component.querySelectorAll( "li:not(:nth-child(1)):not(:last-child)" ) ) {
+      if ( crumb.classList.contains( "more" ) ) {
+        crumb.classList.remove( "more" );
+        crumb.classList.add( "hidden" );
       } else {
-        crumb.classList.remove("hidden");
+        crumb.classList.remove( "hidden" );
       }
     }
   }
 }
 
 let breadcrumbList = [];
-for (let brBreadcrumb of window.document.querySelectorAll(".br-breadcrumb")) {
-  breadcrumbList.push(new BRBreadcrumb("br-breadcrumb", brBreadcrumb));
+for ( let brBreadcrumb of window.document.querySelectorAll( ".br-breadcrumb" ) ) {
+  breadcrumbList.push( new BRBreadcrumb( "br-breadcrumb", brBreadcrumb ) );
 }
 
 export default BRBreadcrumb;
