859626056fa63df924d72ba5ec0022ce33c84b40
Matheus Monteiro <matheus.monteiro@serpro.gov.br>
2020-07-02 18:25:46 -0300

    feat(select): cria comportamento de select

--- src/js/components/select.js
+++ src/js/components/select.js
@@ -2,17 +2,16 @@ class BRSelect {
   constructor(name, component) {
     this.name = name
     this.component = component
-    this.options = this._setOptions()
+    this.options = []
     this.selected = []
+    this._setOptions()
     this._setBehavior()
   }
 
   _setOptions() {
-    const options = []
-    for (const item of this.component.querySelectorAll('.br-list .item .content')) {
-      options.push(item.innerText)
+    for (const content of this.component.querySelectorAll('.br-list.options-list .item .content')) {
+      this.options.push(content.innerText)
     }
-    return options
   }
 
   _setBehavior() {
@@ -24,72 +23,134 @@ class BRSelect {
     }
   }
 
-  _setExpandBehavior() {
-    for (const buttonTrigger of this.component.querySelectorAll('.br-input button[trigger]')) {
-      buttonTrigger.addEventListener('click', (event) => {
-        for (const brCard of this.component.querySelectorAll('.br-card')) {
-          if (brCard.hasAttribute('expanded')) {
-            brCard.removeAttribute('expanded')
-            for (const icon of event.currentTarget.querySelectorAll('svg')) {
-              icon.classList.remove('fa-angle-up')
-              icon.classList.add('fa-angle-down')
-            }
+  _selectItem(event) {
+    for (const content of event.currentTarget.querySelectorAll('.content')) {
+      if (this.component.hasAttribute('multiple')) {
+        this.selected.push(content.innerText)
+        event.currentTarget.classList.add('hidden')
+        this._buildSelectedList(content.innerText)
+        for (const input of this.component.querySelectorAll('.br-input input')) {
+          if (this.selected.length === 1) {
+            input.value = `${this.selected.length} item selecionado`
           } else {
-            brCard.setAttribute('expanded', '')
-            for (const icon of event.currentTarget.querySelectorAll('svg')) {
-              icon.classList.remove('fa-angle-down')
-              icon.classList.add('fa-angle-up')
+            input.value = `${this.selected.length} itens selecionados`
+          }
+        }
+      } else {
+        this.selected = content.innerText
+        for (const item of this.component.querySelectorAll('.br-list.options-list .item')) {
+          if (item === event.currentTarget) {
+            item.setAttribute('selected', '')
+          } else {
+            item.removeAttribute('selected')
+          }
+        }
+        for (const input of this.component.querySelectorAll('.br-input input')) {
+          input.value = content.innerText
+        }
+        this._closeSelect()
+      }
+    }
+  }
+
+  _unselectItem(event) {
+    for (const content of event.currentTarget.querySelectorAll('.content')) {
+      this.options.push(content.innerText)
+      this.selected.slice(this.selected.indexOf(content.innerText))
+    }
+  }
+
+  _buildSelectedList(option) {
+    for (const selectedList of this.component.querySelectorAll('.br-list.selected-list')) {
+      const item = this._createItem(['fas', 'fa-trash'], option)
+      item.setAttribute('selected', '')
+      item.addEventListener('click', (event) => {
+        this.selected.splice(this.selected.indexOf(option), 1)
+        for (const optionItem of this.component.querySelectorAll('.br-list.options-list .item')) {
+          for (const content of optionItem.querySelectorAll('.content')) {
+            if (content.innerText === option) {
+              optionItem.classList.remove('hidden')
             }
           }
         }
+        item.parentElement.removeChild(item)
+        for (const input of this.component.querySelectorAll('.br-input input')) {
+          if (this.selected.length === 0) {
+            input.value = ''
+          } else if (this.selected.length === 1) {
+            input.value = `${this.selected.length} item selecionado`
+          } else {
+            input.value = `${this.selected.length} itens selecionados`
+          }
+        }
       })
+      selectedList.appendChild(item)
+    }
+  }
+
+  _setExpandBehavior() {
+    for (const buttonTrigger of this.component.querySelectorAll('.br-input button[trigger]')) {
+      buttonTrigger.addEventListener('click', (event) => {
+        for (const card of this.component.querySelectorAll('.br-card')) {
+          if (card.hasAttribute('expanded')) {
+            card.removeAttribute('expanded')
+          } else {
+            card.setAttribute('expanded', '')
+          }
+        }
+        for (const icon of event.currentTarget.querySelectorAll('svg')) {
+          if (icon.classList.contains('fa-angle-up')) {
+            icon.classList.remove('fa-angle-up')
+            icon.classList.add('fa-angle-down')
+          } else {
+            icon.classList.remove('fa-angle-down')
+            icon.classList.add('fa-angle-up')
+          }
+        }
+      })
+    }
+  }
+
+  _closeSelect() {
+    for (const card of this.component.querySelectorAll('.br-card')) {
+      card.removeAttribute('expanded', '')
+      for (const icon of this.component.querySelectorAll('.br-input button[trigger] svg')) {
+        icon.classList.remove('fa-angle-up')
+        icon.classList.add('fa-angle-down')
+      }
     }
   }
 
   _setSelectMultipleBehavior() {
-    for (const item of this.component.querySelectorAll('.br-list .item')) {
-      item.addEventListener('click', (event) => {
-        for (const selectedList of this.component.querySelectorAll(
-          '.br-list.selected-options-list'
-        )) {
-          if (event.currentTarget.parentNode.classList.contains('options-list')) {
-            selectedList.appendChild(event.currentTarget)
-          }
-          if (event.currentTarget.parentNode.classList.contains('selected-options-list')) {
-          }
-        }
-        for (const supportIcon of item.querySelectorAll('.support svg')) {
-          supportIcon.classList.remove('fa-plus-circle')
-          supportIcon.classList.add('fa-trash')
-        }
-      })
+    for (const item of this.component.querySelectorAll('.br-list.options-list .item')) {
+      item.addEventListener('click', this._selectItem.bind(this))
     }
   }
 
   _setSelectSimpleBehavior() {
-    for (const item of this.component.querySelectorAll('.br-list .item')) {
-      item.addEventListener('click', (event) => {
-        item.setAttribute('selected', '')
-        for (const option of this.component.querySelectorAll('.br-list .item')) {
-          if (option !== event.currentTarget) {
-            option.removeAttribute('selected')
-          }
-        }
-        for (const option of item.querySelectorAll('.content')) {
-          this.selected[0] = option.innerText
-          for (const input of this.component.querySelectorAll('input')) {
-            input.value = option.innerText
-          }
-        }
-        for (const brCard of this.component.querySelectorAll('.br-card')) {
-          brCard.removeAttribute('expanded')
-          for (const icon of this.component.querySelectorAll('button.icon svg')) {
-            icon.classList.remove('fa-angle-up')
-            icon.classList.add('fa-angle-down')
-          }
-        }
-      })
+    for (const item of this.component.querySelectorAll('.br-list.options-list .item')) {
+      item.addEventListener('click', this._selectItem.bind(this))
     }
+  }
+
+  _createItem(iconClasses, option) {
+    const item = document.createElement('div')
+    item.classList.add('item')
+
+    const icon = document.createElement('i')
+    icon.classList.add(...iconClasses)
+    const support = document.createElement('div')
+    support.classList.add('support')
+    support.appendChild(icon)
+
+    const content = document.createElement('div')
+    content.classList.add('content')
+    content.innerText = option
+
+    item.appendChild(support)
+    item.appendChild(content)
+
+    return item
   }
 }
 
