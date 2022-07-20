7580d12f530bc05b00b68c8335e92c2ecbeabf73
samuel.brandao <samuel.brandao@serpro.gov.br>
2022-07-20 17:27:27 -0300

    refactor(wizard): documentacao jsdoc, ajuste de bug layout

--- src/components/wizard/wizard.js
+++ src/components/wizard/wizard.js
@@ -1,186 +1,219 @@
 import Swipe from '../../partial/js/behavior/swipe'
 class BRWizard {
-  constructor(name, component) {
-    this.name = name
-    this.component = component
-    // DOM elements
-    this.DOMstrings = {
-      stepFormPanelClass: 'wizard-panel',
-      // stepFormPanels: document.querySelectorAll('.wizard-panel'),
-      stepFormPanels: this.component.querySelectorAll('.wizard-panel'),
-      stepNextBtnClass: 'wizard-btn-next',
-      stepPrevBtnClass: 'wizard-btn-prev',
-      // stepsBar: document.querySelector('.wizard-progress'),
-      stepsBar: this.component.querySelector('.wizard-progress'),
-      stepsBarClass: 'wizard-progress',
-      stepsBtnClass: 'wizard-progress-btn',
-      // stepsBtns: document.querySelectorAll(`.wizard-progress-btn`),
-      stepsBtns: this.component.querySelectorAll('.wizard-progress-btn'),
-      // stepsForm: document.querySelector('.wizard-form'),
-      stepsForm: this.component.querySelector('.wizard-form'),
-    }
-    // remove class from a set of items
-    this.removeAttributes = (elemSet, attrName) => {
-      elemSet.forEach((elem) => {
-        elem.removeAttribute(attrName)
-      })
-    }
-    // return exect parent node of the element
-    this.findParent = (elem, parentClass) => {
-      let currentNode = elem
-      while (!currentNode.classList.contains(parentClass)) {
-        currentNode = currentNode.parentNode
-      }
-      return currentNode
-    }
-    // get active button step number
-    this.getActiveStep = (elem) => {
-      return Array.from(this.DOMstrings.stepsBtns).indexOf(elem)
-    }
-    // set all steps before clicked (and clicked too) to active
-    this.setActiveStep = function (activeStepNum) {
-      // remove active state from all the state
-      this.removeAttributes(this.DOMstrings.stepsBtns, 'active')
-      // this.removeAttributes(this.DOMstrings.stepsBtns, 'disabled')
+	/**
+	 * Instância do componente
+	 * @param {string} name - nome do componente
+	 * @param {object} component - referencia ao objeto do DOM
+	 **/
+	constructor(name, component) {
+		this.name = name
+		this.component = component
+		this.DOMstrings = {
+			stepFormPanelClass: 'wizard-panel',
+			stepFormPanels: this.component.querySelectorAll('.wizard-panel'),
+			stepNextBtnClass: 'wizard-btn-next',
+			stepPrevBtnClass: 'wizard-btn-prev',
+			stepsBar: this.component.querySelector('.wizard-progress'),
+			stepsBarClass: 'wizard-progress',
+			stepsBtnClass: 'wizard-progress-btn',
+			stepsBtns: this.component.querySelectorAll('.wizard-progress-btn'),
+			stepsForm: this.component.querySelector('.wizard-form'),
+		}
+		/**
+		 * Retira o atributo de uma lista de elementos
+		 * @param {object[]} elementSet - Lista de objetos
+		 * @param {string} attrName - Nome do atribbuto
+		 */
+		this.removeAttributes = (elemSet, attrName) => {
+			elemSet.forEach((elem) => {
+				elem.removeAttribute(attrName)
+			})
+		}
 
-      // set picked items to active
-      this.DOMstrings.stepsBtns.forEach((elem, index) => {
-        if (index === activeStepNum) {
-          elem.removeAttribute('disabled')
-          elem.setAttribute('active', '')
-        }
-        // if (index > activeStepNum) {
-        //   elem.setAttribute('disabled', 'disabled')
-        // }
-      })
-    }
-    // get active panel
-    this.getActivePanel = () => {
-      let activePanel
-      this.DOMstrings.stepFormPanels.forEach((elem) => {
-        if (elem.hasAttribute('active')) {
-          activePanel = elem
-        }
-      })
-      return activePanel
-    }
-    // open active panel (and close unactive panels)
-    this.setActivePanel = (activePanelNum) => {
-      // remove active class from all the panels
-      this.removeAttributes(this.DOMstrings.stepFormPanels, 'active')
-      // show active panel
-      this.DOMstrings.stepFormPanels.forEach((elem, index) => {
-        if (index === activePanelNum) {
-          elem.setAttribute('active', '')
-        }
-      })
-    }
-    this.setStepsNum = () => {
-      this.DOMstrings.stepsBtns.forEach((elem, index) => {
-        elem.setAttribute('step', index + 1)
-      })
-    }
-    this.setStep = (num) => {
-      const activeStep = num <= this.DOMstrings.stepsBtns.length ? num - 1 : 0
-      this.setActiveStep(activeStep)
-      this.setActivePanel(activeStep)
-    }
-    this.collapseSteps = () => {
-      this.component.setAttribute('collapsed', '')
-    }
-    this.expandSteps = () => {
-      this.component.removeAttribute('collapsed')
-    }
-    this._setBehavior()
-  }
+		/**
+		 * Retorna o elemento pai do objeto com a classe de referência
+		 * @param {object} elem - Lista de objetos
+		 * @param {string} parentClass - nome da classe de referência
+		 * @returns {object}
+		 */
+		this.findParent = (elem, parentClass) => {
+			let currentNode = elem
+			while (!currentNode.classList.contains(parentClass)) {
+				currentNode = currentNode.parentNode
+			}
+			return currentNode
+		}
 
-  _setBehavior() {
-    // STEPS BAR CLICK FUNCTION
-    this.DOMstrings.stepsBar.addEventListener('click', (e) => {
-      // check if click target is a step button
-      const eventTarget = e.target
-      if (!eventTarget.classList.contains(`${this.DOMstrings.stepsBtnClass}`)) {
-        e.target.parentNode.click()
-        return
-      }
-      // get active button step number
-      const activeStep = this.getActiveStep(eventTarget)
-      // set all steps before clicked (and clicked too) to active
-      this.setActiveStep(activeStep)
-      // open active panel
-      this.setActivePanel(activeStep)
-    })
-    // PREV/NEXT BTNS CLICK
-    this.DOMstrings.stepsForm.addEventListener('click', (e) => {
-      const eventTarget = e.target
-      // check if we clicked on `PREV` or NEXT` buttons
-      if (
-        !(
-          eventTarget.classList.contains(
-            `${this.DOMstrings.stepPrevBtnClass}`
-          ) ||
-          eventTarget.classList.contains(`${this.DOMstrings.stepNextBtnClass}`)
-        )
-      ) {
-        return
-      }
-      // find active panel
-      const activePanel = this.findParent(
-        eventTarget,
-        `${this.DOMstrings.stepFormPanelClass}`
-      )
-      let activePanelNum = Array.from(this.DOMstrings.stepFormPanels).indexOf(
-        activePanel
-      )
-      // set active step and active panel onclick
-      if (
-        eventTarget.classList.contains(`${this.DOMstrings.stepPrevBtnClass}`)
-      ) {
-        activePanelNum -= 1
-        activePanel.style.left = '1%'
-      } else {
-        activePanelNum += 1
-        activePanel.style.left = '-1%'
-      }
-      this.setActiveStep(activePanelNum)
-      this.setActivePanel(activePanelNum)
-    })
-    // Set Steps label number
-    this.setStepsNum()
-    // Set default active step
-    if (this.component.hasAttribute('step')) {
-      this.setStep(this.component.getAttribute('step'))
-    }
-    // set steps buttons grid style if it needs to scroll horizontaly
-    if (
-      this.component.hasAttribute('scroll') &&
-      !this.component.hasAttribute('vertical')
-    ) {
-      const stepsWidth =
-        Math.round(100 / this.DOMstrings.stepsBtns.length) - 0.5
-      this.DOMstrings.stepsBar.style.gridTemplateColumns = `repeat(auto-fit, minmax(100px, ${stepsWidth}% ))`
-    }
-    // Swipe
-    const dispatcher = new Swipe(this.DOMstrings.stepsBar)
-    if (this.component.hasAttribute('vertical')) {
-      dispatcher.on('SWIPE_LEFT', () => {
-        this.collapseSteps()
-      })
-      dispatcher.on('SWIPE_RIGHT', () => {
-        this.expandSteps()
-      })
-      this.DOMstrings.stepsForm.ontouchstart = () => {
-        this.collapseSteps()
-      }
-    } else {
-      this.DOMstrings.stepsBar.ontouchstart = () => {
-        this.expandSteps()
-      }
-      this.DOMstrings.stepsForm.ontouchstart = () => {
-        this.collapseSteps()
-      }
-    }
-  }
+		/**
+		 * Retorna o índice do elemento botão de passo
+		 * @param {object} elem - botão de passo
+		 * @returns {number}
+		 */
+		this.getActiveStep = (elem) => {
+			return Array.from(this.DOMstrings.stepsBtns).indexOf(elem)
+		}
+
+		/**
+		 * Define o estado do botão ativo e limpa os demais estados dos botões
+		 * @param {number} activeStepNum - número do botão ativo
+		 */
+		this.setActiveStep = function (activeStepNum) {
+			this.removeAttributes(this.DOMstrings.stepsBtns, 'active')
+			this.DOMstrings.stepsBtns.forEach((elem, index) => {
+				if (index === activeStepNum) {
+					elem.removeAttribute('disabled')
+					elem.setAttribute('active', '')
+				}
+			})
+		}
+
+		/**
+		 * Retorna o índice do painel ativo
+		 * @returns {number}
+		 */
+		this.getActivePanel = () => {
+			let activePanel
+			this.DOMstrings.stepFormPanels.forEach((elem) => {
+				if (elem.hasAttribute('active')) {
+					activePanel = elem
+				}
+			})
+			return activePanel
+		}
+
+		/**
+		 * Abre o painel ativo e fecha paineis inativos
+		 * @param {number} activePanelNum - numero do painel
+		 */
+		this.setActivePanel = (activePanelNum) => {
+			// remove active class from all the panels
+			this.removeAttributes(this.DOMstrings.stepFormPanels, 'active')
+			// show active panel
+			this.DOMstrings.stepFormPanels.forEach((elem, index) => {
+				if (index === activePanelNum) {
+					elem.setAttribute('active', '')
+				}
+			})
+		}
+		this.setStepsNum = () => {
+			this.DOMstrings.stepsBtns.forEach((elem, index) => {
+				elem.setAttribute('step', index + 1)
+			})
+		}
+		this.setStep = (num) => {
+			const activeStep =
+				num <= this.DOMstrings.stepsBtns.length ? num - 1 : 0
+			this.setActiveStep(activeStep)
+			this.setActivePanel(activeStep)
+		}
+		this.collapseSteps = () => {
+			this.component.setAttribute('collapsed', '')
+		}
+		this.expandSteps = () => {
+			this.component.removeAttribute('collapsed')
+		}
+		this._setBehavior()
+	}
+
+	/**
+	 * Define os comportamentos do componente
+	 * @private
+	 */
+	_setBehavior() {
+		// STEPS BAR CLICK FUNCTION
+		this.DOMstrings.stepsBar.addEventListener('click', (e) => {
+			// testa se o alvo é um botão de passo
+			const eventTarget = e.target
+			if (
+				!eventTarget.classList.contains(
+					`${this.DOMstrings.stepsBtnClass}`
+				)
+			) {
+				e.target.parentNode.click()
+				return
+			}
+			// get active button step number
+			const activeStep = this.getActiveStep(eventTarget)
+			// set all steps before clicked (and clicked too) to active
+			this.setActiveStep(activeStep)
+			// open active panel
+			this.setActivePanel(activeStep)
+		})
+		// PREV/NEXT BTNS CLICK
+		this.DOMstrings.stepsForm.addEventListener('click', (e) => {
+			const eventTarget = e.target
+			// check if we clicked on `PREV` or NEXT` buttons
+			if (
+				!(
+					eventTarget.classList.contains(
+						`${this.DOMstrings.stepPrevBtnClass}`
+					) ||
+					eventTarget.classList.contains(
+						`${this.DOMstrings.stepNextBtnClass}`
+					)
+				)
+			) {
+				return
+			}
+			// find active panel
+			const activePanel = this.findParent(
+				eventTarget,
+				`${this.DOMstrings.stepFormPanelClass}`
+			)
+			let activePanelNum = Array.from(
+				this.DOMstrings.stepFormPanels
+			).indexOf(activePanel)
+			// set active step and active panel onclick
+			if (
+				eventTarget.classList.contains(
+					`${this.DOMstrings.stepPrevBtnClass}`
+				)
+			) {
+				activePanelNum -= 1
+				activePanel.style.left = '1%'
+			} else {
+				activePanelNum += 1
+				activePanel.style.left = '-1%'
+			}
+			this.setActiveStep(activePanelNum)
+			this.setActivePanel(activePanelNum)
+		})
+		// Set Steps label number
+		this.setStepsNum()
+		// Set default active step
+		if (this.component.hasAttribute('step')) {
+			this.setStep(this.component.getAttribute('step'))
+		}
+		// set steps buttons grid style if it needs to scroll horizontaly
+		if (
+			this.component.hasAttribute('scroll') &&
+			!this.component.hasAttribute('vertical')
+		) {
+			const stepsWidth =
+				Math.round(100 / this.DOMstrings.stepsBtns.length) - 0.5
+			this.DOMstrings.stepsBar.style.gridTemplateColumns = `repeat(auto-fit, minmax(100px, ${stepsWidth}% ))`
+		}
+		// Swipe
+		const dispatcher = new Swipe(this.DOMstrings.stepsBar)
+		if (this.component.hasAttribute('vertical')) {
+			dispatcher.on('SWIPE_LEFT', () => {
+				this.collapseSteps()
+			})
+			dispatcher.on('SWIPE_RIGHT', () => {
+				this.expandSteps()
+			})
+			this.DOMstrings.stepsForm.ontouchstart = () => {
+				this.collapseSteps()
+			}
+		} else {
+			this.DOMstrings.stepsBar.ontouchstart = () => {
+				this.expandSteps()
+			}
+			this.DOMstrings.stepsForm.ontouchstart = () => {
+				this.collapseSteps()
+			}
+		}
+	}
 }
 
 export default BRWizard
