deaf36cdc7617bac3252d23011dc3f79a7b39866
samuel.brandao <samuel.brandao@serpro.gov.br>
2022-07-26 16:24:21 -0300

    refactor(step): revisao js e comentarios jsdoc

--- src/components/step/step.js
+++ src/components/step/step.js
@@ -1,137 +1,132 @@
+/** Classe para instanciar um objeto BRStep */
 class BRStep {
-  constructor(name, component) {
-    /**
-     * Instancia um componente carousel
-     * @param {string} name - Component name
-     * @param {object} component - DOM root Object reference
-     * @property {number} activeStepNum - Número do palco ativo
-     * @property {array} DOMStrings - instancia dos elementos internos do componente
-     */
-    this.name = name
-    this.component = component
-    this.activeStepNum = 0
-    // DOM elements
-    this.DOMstrings = {
-      stepsBarClass: 'step-progress',
-      stepsBar: this.component.querySelector('.step-progress'),
-      stepsBtnClass: 'step-progress-btn',
-      stepsBtns: this.component.querySelectorAll('.step-progress-btn'),
-    }
+	constructor(name, component) {
+		/**
+		 * Instancia do componente
+		 * @param {string} name - Nome do componente em minúsculo
+		 * @param {object} component - Objeto referenciando a raiz do componente DOM
+		 * @property {number} activeStepNum - Número do palco ativo
+		 * @property {array} DOMStrings - instancia dos elementos internos do componente
+		 */
+		this.name = name
+		this.component = component
+		this.activeStepNum = 0
+		this.DOMstrings = {
+			stepsBarClass: 'step-progress',
+			stepsBar: this.component.querySelector('.step-progress'),
+			stepsBtnClass: 'step-progress-btn',
+			stepsBtns: this.component.querySelectorAll('.step-progress-btn'),
+		}
 
-    /**
-     * Remove class from a set of items
-     * @param {object} elemSet - Elelment's List
-     * @param {string} button - Attribute name
-     */
-    this.removeAttributes = (elemSet, attrName) => {
-      elemSet.forEach((elem) => {
-        elem.removeAttribute(attrName)
-      })
-    }
+		/**
+		 * Remove uma classe css de uma lista de elementos
+		 * @param {object} elemSet - Lista de elementos
+		 * @param {string} button - Nome do atributo
+		 */
+		this.removeAttributes = (elemSet, attrName) => {
+			elemSet.forEach((elem) => {
+				elem.removeAttribute(attrName)
+			})
+		}
 
-    /**
-     * Return exect parent node of the element
-     * @param {object} elem - element
-     * @param {string} button - father's class
-     */
-    this.findParent = (elem, parentClass) => {
-      let currentNode = elem
-      while (!currentNode.classList.contains(parentClass)) {
-        currentNode = currentNode.parentNode
-      }
-      return currentNode
-    }
+		/**
+		 * Retorna o nó pai do elemento com o nome da classe específica
+		 * @param {object} elem - Referência ao elemento
+		 * @param {string} parentClass - Nome da classe pai
+		 * @returns {object} Referência ao elemento pai
+		 */
+		this.findParent = (elem, parentClass) => {
+			let currentNode = elem
+			while (!currentNode.classList.contains(parentClass)) {
+				currentNode = currentNode.parentNode
+			}
+			return currentNode
+		}
 
-    /** get active button step number
-     * @param {object} elem - element
-     */
-    this.getActiveStep = (elem) => {
-      return Array.from(this.DOMstrings.stepsBtns).indexOf(elem)
-    }
+		/** Retorna o número do passo de referencia
+		 * @param {object} elem - Referência ao botão de passo
+		 * @returns {number} Número do passo
+		 */
+		this.getActiveStep = (elem) => {
+			return Array.from(this.DOMstrings.stepsBtns).indexOf(elem)
+		}
 
-    /** Set all steps before clicked (and clicked too) to active
-     * @param {number} num - step number
-     */
-    this.setActiveStep = function (num) {
-      // remove active state from all the state
-      this.removeAttributes(this.DOMstrings.stepsBtns, 'active')
-      // this.removeAttributes(this.DOMstrings.stepsBtns, 'disabled')
+		/** Define o número do passo de referência como ativo
+		 * @param {number} num - numero do passo de referência
+		 */
+		this.setActiveStep = function (num) {
+			this.removeAttributes(this.DOMstrings.stepsBtns, 'active')
+			this.DOMstrings.stepsBtns.forEach((elem, index) => {
+				if (index === num) {
+					elem.removeAttribute('disabled')
+					elem.setAttribute('active', '')
+				}
+			})
+			this.activeStepNum = num
+		}
 
-      // set picked items to active
-      this.DOMstrings.stepsBtns.forEach((elem, index) => {
-        if (index === num) {
-          elem.removeAttribute('disabled')
-          elem.setAttribute('active', '')
-        }
-      })
-      this.activeStepNum = num
-    }
+		/**
+		 * Mostra os números dos rótulos dos passos
+		 */
+		this.setStepsNum = () => {
+			this.DOMstrings.stepsBtns.forEach((elem, index) => {
+				const img = elem.querySelector('.step-icon')
+				const text = this.component.getAttribute('data-type') === 'text'
+				if (text) {
+					elem.setAttribute(
+						'step-num',
+						`${index + 1}/${this.DOMstrings.stepsBtns.length}`
+					)
+				} else if (img) {
+					elem.setAttribute('step-num', '')
+				} else elem.setAttribute('step-num', index + 1)
+			})
+		}
 
-    /**
-     * Prints numbers labels into steps
-     */
-    this.setStepsNum = () => {
-      this.DOMstrings.stepsBtns.forEach((elem, index) => {
-        const img = elem.querySelector('.step-icon')
-        const text = this.component.getAttribute('data-type') === 'text'
-        if (text) {
-          elem.setAttribute(
-            'step-num',
-            `${index + 1}/${this.DOMstrings.stepsBtns.length}`
-          )
-        } else if (img) {
-          elem.setAttribute('step-num', '')
-        } else elem.setAttribute('step-num', index + 1)
-      })
-    }
+		/** Testa se o passo está dentro do escopo e o define como ativo
+		 * @param {number} num - step number
+		 */
+		this.setStep = (num) => {
+			const activeStep =
+				num <= this.DOMstrings.stepsBtns.length ? num - 1 : 0
+			this.setActiveStep(activeStep)
+		}
 
-    /** Filters active step into range
-     * @param {number} num - step number
-     */
-    this.setStep = (num) => {
-      const activeStep = num <= this.DOMstrings.stepsBtns.length ? num - 1 : 0
-      this.setActiveStep(activeStep)
-    }
+		this._setBehavior()
+	}
 
-    this._setBehavior()
-  }
+	/**
+	 * Define comportamentos do componente
+	 * @private
+	 */
+	_setBehavior() {
+		this.DOMstrings.stepsBar.addEventListener('click', (e) => {
+			const eventTarget = e.target
+			if (
+				!eventTarget.classList.contains(
+					`${this.DOMstrings.stepsBtnClass}`
+				)
+			) {
+				e.target.parentNode.click()
+				return
+			}
+			const activeStepNum = this.getActiveStep(eventTarget)
+			this.setActiveStep(activeStepNum)
+		})
 
-  /**
-   * Set component behaviors
-   * @private
-   */
-  _setBehavior() {
-    // STEPS BAR CLICK FUNCTION
-    this.DOMstrings.stepsBar.addEventListener('click', (e) => {
-      // check if click target is a step button
-      const eventTarget = e.target
-      if (!eventTarget.classList.contains(`${this.DOMstrings.stepsBtnClass}`)) {
-        e.target.parentNode.click()
-        return
-      }
-      // get active button step number
-      const activeStepNum = this.getActiveStep(eventTarget)
-      // set all steps before clicked (and clicked too) to active
-      this.setActiveStep(activeStepNum)
-    })
+		this.setStepsNum()
+		if (this.component.hasAttribute('data-initial')) {
+			this.setStep(this.component.getAttribute('data-initial'))
+		} else this.setStep(1)
 
-    // Set Steps label number
-    this.setStepsNum()
-    // Set default active step
-    if (this.component.hasAttribute('data-initial')) {
-      this.setStep(this.component.getAttribute('data-initial'))
-    } else this.setStep(1)
-
-    // set steps buttons grid style if it needs to scroll horizontaly
-    if (
-      !this.component.classList.contains('vertical') &&
-      !this.component.hasAttribute('data-scroll')
-    ) {
-      const stepsWidth =
-        Math.round(100 / this.DOMstrings.stepsBtns.length) - 0.5
-      // this.DOMstrings.stepsBar.style.gridTemplateColumns = `repeat(auto-fit, minmax(100px, ${stepsWidth}% ))`
-    }
-  }
+		if (
+			!this.component.classList.contains('vertical') &&
+			!this.component.hasAttribute('data-scroll')
+		) {
+			const stepsWidth =
+				Math.round(100 / this.DOMstrings.stepsBtns.length) - 0.5
+		}
+	}
 }
 
 export default BRStep
