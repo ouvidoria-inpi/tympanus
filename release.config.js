9e60e4e0a3101c6525983592509bed39f15f9f65
Rafael Diego Gomes de Oliveira <rafael.gomes@serpro.gov.br>
2022-12-09 11:50:48 -0300

    ci: substitui o standard-version pelo semantic-release

---
+++ release.config.js
@@ -0,0 +1,11 @@
+const sharedConfig = require('@govbr-ds/release-config')
+
+module.exports = {
+  branches: [...sharedConfig.branches],
+  plugins: [
+    sharedConfig.plugins.commitAnalyzer,
+    sharedConfig.plugins.releaseNotes,
+    sharedConfig.plugins.changelog,
+    sharedConfig.plugins.gitlab
+  ],
+}
