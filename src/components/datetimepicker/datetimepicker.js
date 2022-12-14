740c59442fde8243b971441f4a24c4ef5f6a0859
tiagoaos <tiago-alexandre.silva@serpro.gov.br>
2022-12-13 23:52:20 -0300

    fix(datepicker): altera o icone up and down

--- src/components/datetimepicker/datetimepicker.js
+++ src/components/datetimepicker/datetimepicker.js
@@ -3,94 +3,118 @@ const Brazilian = require('flatpickr/dist/l10n/pt').default.pt
 
 /** Classe para instanciar um objeto BRDateTimePicker*/
 class BRDateTimePicker {
-	/**
-	 * Instancia do objeto
-	 * @param {string} name - Nome do componente em minúsculo
-	 * @param {object} component - Objeto referenciando a raiz do componente DOM
-	 * @param {string} config - atributo de configuração do componente
-	 */
-	constructor(name, component, config) {
-		this.name = name
-		this.component = component
-		// localization global
-		flatpickr.localize(Brazilian)
+  /**
+   * Instancia do objeto
+   * @param {string} name - Nome do componente em minúsculo
+   * @param {object} component - Objeto referenciando a raiz do componente DOM
+   * @param {string} config - atributo de configuração do componente
+   */
+  constructor(name, component, config) {
+    this.name = name
+    this.component = component
+    // localization global
+    flatpickr.localize(Brazilian)
 
-		this.configAttribute = this.component.getAttribute(
-			'datetimepicker-config'
-		)
+    this.configAttribute = this.component.getAttribute('datetimepicker-config')
 
-		if (this.configAttribute) {
-			// Transforma o atributo em um objeto
-			const properties = this.configAttribute.split(',')
-			this.obj = []
-			properties.forEach((element) => {
-				const tup = element.split(':')
-				this.obj[tup[0]] = tup[1].replaceAll("'", '').trim() // eslint-disable-line
+    if (this.configAttribute) {
+      // Transforma o atributo em um objeto
+      const properties = this.configAttribute.split(',')
+      this.obj = []
+      properties.forEach((element) => {
+        const tup = element.split(':')
+        this.obj[tup[0]] = tup[1].replaceAll("'", '').trim() // eslint-disable-line
 
-				this.saida = this.obj
-			}, this)
+        this.saida = this.obj
+      }, this)
 
-			this.config = this.obj
-		} else {
-			this.config = config
-		}
+      this.config = this.obj
+    } else {
+      this.config = config
+    }
 
-		this._buildDateTimePicker()
-	}
+    this._buildDateTimePicker()
+  }
 
-	/**
-	 * Formata o componente e monta instância flatpickr
-	 * @private
-	 */
-	_buildDateTimePicker() {
-		let format = 'd/m/Y'
-		let time = false
-		let noCalendar = false
-		switch (this.component.getAttribute('data-type')) {
-			case 'date':
-				format = 'd/m/Y'
-				time = false
-				noCalendar = false
-				break
-			case 'time':
-				format = 'H:i'
-				time = true
-				noCalendar = true
-				break
-			case 'datetime-local':
-				format = 'd/m/Y H:i'
-				time = true
-				noCalendar = false
-				break
-			default:
-				format = 'd/m/Y'
-				time = false
-				noCalendar = false
-				break
-		}
+  /**
+   * Formata o componente e monta instância flatpickr
+   * @private
+   */
+  _buildDateTimePicker() {
+    let format = 'd/m/Y'
+    let time = false
+    let noCalendar = false
+    switch (this.component.getAttribute('data-type')) {
+      case 'date':
+        format = 'd/m/Y'
+        time = false
+        noCalendar = false
+        break
+      case 'time':
+        format = 'H:i'
+        time = true
+        noCalendar = true
+        break
+      case 'datetime-local':
+        format = 'd/m/Y H:i'
+        time = true
+        noCalendar = false
+        break
+      default:
+        format = 'd/m/Y'
+        time = false
+        noCalendar = false
+        break
+    }
 
-		this.config_native = {
-			dateFormat: format,
-			disableMobile: 'true',
-			enableTime: time,
-			minuteIncrement: 1,
+    this.config_native = {
+      dateFormat: format,
+      disableMobile: 'true',
+      enableTime: time,
+      minuteIncrement: 1,
 
-			mode: this.component.getAttribute('data-mode'),
-			nextArrow:
-				'<button class="br-button circle small" type="button"><i class="fas fa-chevron-right"></i></button>',
-			noCalendar: noCalendar,
-			prevArrow:
-				'<button class="br-button circle small" type="button"><i class="fas fas fa-chevron-left"></i></button>',
-			time_24hr: true,
-			wrap: true,
-		}
-		this.config_flatpick = Object.assign(this.config, this.config_native)
+      mode: this.component.getAttribute('data-mode'),
+      nextArrow:
+        '<button class="br-button circle small" type="button"><i class="fas fa-chevron-right"></i></button>',
+      noCalendar: noCalendar,
+      prevArrow:
+        '<button class="br-button circle small" type="button"><i class="fas fas fa-chevron-left"></i></button>',
+      time_24hr: true,
+      wrap: true,
+      
+    }
+    this.config_flatpick = Object.assign(this.config, this.config_native)
 
-		this.calendar = flatpickr(
-			this.component,
-			Object.assign(this.config, this.config_native)
-		)
-	}
+    this.calendar = flatpickr(
+      this.component,
+      Object.assign(this.config, this.config_native)
+    )
+
+    
+
+    this.calendar.config.onOpen.push(function() {
+    
+    
+      // <i class="fas fa-angle-right"></i>
+      const icone = document.createElement("i");
+      icone.classList.add("fas", "fa-angle-right");
+      
+      document.querySelectorAll(".arrowUp").forEach(function(element){
+        
+        
+        //element.replaceWith(icone);
+        element.classList.add("fas", "fa-chevron-up");
+        element.parentElement.appendChild(icone)
+        
+      });
+      document.querySelectorAll(".arrowDown").forEach(function(element){
+        
+        element.classList.add("fas", "fa-chevron-down");
+        
+      });
+      
+     } );
+  }
 }
 
 export default BRDateTimePicker
