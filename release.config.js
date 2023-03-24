210e0b195b65ec303e1e5a78d08861b62bfe5cee
Adriano <adriano.albuquerque@serpro.gov.br>
2023-03-24 19:28:16 +0000

    feat: atualiza links da wiki
    Esse commit é um squash de:
    * chore: remove arquivo desnecessário
    
    * chore: remove arquivo desnecessário
    
    * feat: atualiza links da wiki
    
    Foi feito pelo MR: govbr-ds/dev/govbr-ds-dev-core!117

--- release.config.js
+++ release.config.js
@@ -28,7 +28,7 @@ module.exports = {
         notifyOnFail: false,
         onSuccessTemplate: {
           content:
-            'A versão v$npm_package_version do nosso pacote $package_name já está disponível no [NPMJS](https://www.npmjs.com/package/$package_name \'Acessar página do pacote NPM\')! :partying_face: \n\n Para conhecer melhor as novidades: \n\n :book: [Página de releases]($repo_url/-/releases "Releases") \n\n :computer: [Repositório no Gitlab]($repo_url) \n\n Deseja contribuir com o Design System? Consulte a nossa [wiki](https://govbr-ds.gitlab.io/govbr-ds-wiki/ "Wiki GOVBR-DS") para mais detalhes.',
+            'A versão v$npm_package_version do nosso pacote $package_name já está disponível no [NPMJS](https://www.npmjs.com/package/$package_name \'Acessar página do pacote NPM\')! :partying_face: \n\n Para conhecer melhor as novidades: \n\n :book: [Página de releases]($repo_url/-/releases "Releases") \n\n :computer: [Repositório no Gitlab]($repo_url) \n\n Deseja contribuir com o Design System? Consulte a nossa [wiki](https://gov.br/ds/wiki/ "Wiki GOVBR-DS") para mais detalhes.',
           embeds: [
             {
               color: 16736031,
